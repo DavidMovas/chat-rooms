38fbc9383ca462ffae0daf668839db933c17329f
Nofre <davidprogram417@gmail.com>
2024-11-14 23:01:04 +0100

    added config implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/DavidMovas/chat-rooms
 
 go 1.23.0
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/caarlos0/env/v11 v11.2.2 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
+
+func NewConfig() (*Config, error) {
+	_ = godotenv.Load()
+
+	var c Config
+
+	if err := env.Parse(&c); err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+
+	return &c, nil
+}
+
+type Config struct {
+	LogLevel string `env:"LOG_LEVEL" envDefault:"warn"`
+	Port     int    `env:"PORT" envDefault:"55555"`
+}
