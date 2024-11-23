591185d9c237c33f2c5d602b2938259b7684f1d8
Nofre <davidprogram417@gmail.com>
2024-11-23 01:10:58 +0100

    created gRPC interceptor for logger

---
+++ internal/log/interceptor.go
@@ -0,0 +1,37 @@
+package log
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log/slog"
+)
+
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		attrs := []any{slog.String("method", info.FullMethod)}
+		logger := slog.Default().With(attrs...)
+
+		ctx = WithLogger(ctx, logger)
+		return handler(ctx, req)
+	}
+}
+
+type wrapperStream struct {
+	grpc.ServerStream
+	ctx context.Context
+}
+
+func (s *wrapperStream) Context() context.Context {
+	return s.ctx
+}
+
+func StreamServerInterceptor() grpc.StreamServerInterceptor {
+	return func(src interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		attrs := []any{slog.String("method", info.FullMethod)}
+		logger := slog.Default().With(attrs...)
+
+		ctx := WithLogger(stream.Context(), logger)
+		wrapped := &wrapperStream{ServerStream: stream, ctx: ctx}
+		return handler(src, wrapped)
+	}
+}
